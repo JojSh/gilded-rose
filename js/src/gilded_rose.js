d9bc757080375cc412b592c1e7ea157c2dfbf401
Joseph Sharratt <joseph.sharratt@gmail.com>
2020-06-27 18:10:50 +0100

    Refactor first pass

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,55 +1,57 @@
-function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+class Item {
+  constructor(name, sell_in, quality) {
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
+  }
 }
 
-var items = []
+let items = []
+
+const specialStock = {
+  'Aged Brie': item => ageBrie(item),
+  'Sulfuras, Hand of Ragnaros': () => {},
+  'Backstage passes to a TAFKAL80ETC concert': item => ageConcertTickets(item),
+}
+
+function improveOnce(item) {
+  if (item.quality < 50) item.quality = item.quality + 1
+}
+
+function degrade(item) {
+  degradeOnce(item)
+  if (item.sell_in <= 0) degradeOnce(item)
+  decrementSellIn(item)
+}
+
+function degradeOnce(item) {
+  if (item.quality > 0) item.quality = item.quality - 1
+}
+
+function decrementSellIn(item) {
+  item.sell_in = item.sell_in - 1
+}
+
+function setQualityToZero(item) {
+  item.quality = 0
+}
+
+function ageBrie(item) {
+  improveOnce(item)
+  decrementSellIn(item)
+}
+
+function ageConcertTickets(item) {
+  improveOnce(item)
+  if (item.sell_in <= 10) improveOnce(item)
+  if (item.sell_in <= 5) improveOnce(item)
+  if (item.sell_in <= 0) setQualityToZero(item)
+  decrementSellIn(item)
+}
 
 function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+  items.forEach(item => {
+    const { name } = item;
+    !specialStock[name] ? degrade(item) : specialStock[name](item);
+  })
 }
